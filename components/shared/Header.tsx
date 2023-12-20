66c789d1e04cf57d7629239dce9bf3f6003c23f0
Elmin Didic <35606145+ElminD@users.noreply.github.com>
2023-12-20 12:18:28 -0600

    drunk cy

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -12,7 +12,7 @@ const Header = () => {
         <div className="wrapper flex items-center justify-between">
             <Link href="/" className="w-36"> 
                 <Image 
-                 src="/assets/images/logo.svg" width={128} height={38}
+                 src="/assets/images/drunkcy.svg" width={128} height={38}
                  alt="Bar Buzz Ames Logo"
                 />
             </Link>
