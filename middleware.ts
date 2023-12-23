56bea88952865328e7f8293b8e55a531dea15a9c
Elmin Didic <35606145+ElminD@users.noreply.github.com>
2023-12-22 20:44:39 -0600

    fix

--- middleware.ts
+++ middleware.ts
@@ -7,7 +7,6 @@ export default authMiddleware({
     '/api/webhook/clerk',
     '/api/webhook/stripe',
     '/api/uploadthing',
-    '/_vercel/insights/*'
   ],
   ignoredRoutes: [
     '/api/webhook/clerk',
