2cada30786382c5b5aac99a9fb55db6e4c62fb91
Elmin Didic <35606145+ElminD@users.noreply.github.com>
2024-01-18 17:30:35 -0600

    revert

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -35,6 +35,8 @@ const EventForm = ({ userId, type, event, eventId }: EventFormProps) => {
   const initialValues = event && type === 'Update' 
     ? { 
       ...event, 
+      startDateTime: new Date(event.startDateTime), 
+      endDateTime: new Date(event.endDateTime) 
     }
     : eventDefaultValues;
   const router = useRouter();
